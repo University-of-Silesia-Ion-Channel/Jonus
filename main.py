69c5b53c2b2c52351e698f618f93a3e7ec125414
Piotr Mika <piotrmika97@protonmail.com>
2024-12-20 09:22:56 +0100

    removed ipywidgets from main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from monostable_model import ion_channel_model, model_force_square, random_force_gauss
-from ipywidgets import interact
+from monostable_model import ion_channel_model
 
 
 if __name__ == '__main__':
-    interact(ion_channel_model(model_force=model_force_square, random_force=random_force_gauss), D=(0, 1, 0.01))
+    ion_channel_model(D=0.5)
 exit()
