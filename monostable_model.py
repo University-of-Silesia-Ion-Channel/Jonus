6276c30d407c722711ab79a99a33f8ef6b846e86
Piotr Mika <piotrmika97@protonmail.com>
2024-12-30 14:11:29 +0100

    Levy tests

--- monostable_model.py
+++ monostable_model.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from random import gauss, choice
+from scipy.stats import levy_stable
+
 
 def random_force_gauss(D, h):
     return (h * D) ** 0.5 * gauss(0, 1)  # losowa siła
 
 def model_force_square(x, a, b):
     return -a*(x - b)
+
+def random_force_levy(D, h, alpha=2, beta=0., loc=0., scale=1.):
+    r = levy_stable.rvs(alpha=alpha, beta=beta, loc=loc, scale=scale)
+    return (h * D) ** 0.5 * r  # losowa siła
 
 def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0.01, records=50000, model_force=model_force_square, random_force=random_force_gauss):
     t = 0
@@ -17,7 +23,6 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
     # losujemy tau, czyli jak długo kanał jest O/Z
     tau = np.random.exponential(b[1])
     while t < records:
-    # print(self.x[t - 1] + self.F(self.x[t - 1], a, b) * delta_t)
         x = np.append(
         x, (
             x[t - 1] +
@@ -35,5 +40,6 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
                 b=closed
             tau = np.random.exponential(b[1])
 
+    plt.clf()
     plt.plot(times, x)
     plt.show()
