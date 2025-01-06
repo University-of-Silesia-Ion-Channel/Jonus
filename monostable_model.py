959f0cc1dd88caa0cb547655a868c6581a8173af
Piotr Mika <piotrmika97@protonmail.com>
2025-01-06 08:40:11 +0100

    Fixed interactable, optimized code a little bit

--- monostable_model.py
+++ monostable_model.py
@@ -4,22 +4,24 @@ from random import gauss, choice
 from scipy.stats import levy_stable
 
 
-def random_force_gauss(D, h, **kwargs):
-    return (h * D) ** 0.5 * gauss(0, 1)  # losowa siła
+def random_force_gauss(D, h, records, args):
+
+    return (h * D) ** 0.5 * gauss(0, 1, size=records)  # losowa siła
 
 def model_force_square(x, a, b):
     return -a*(x - b)
 
-def random_force_levy(D, h, alpha=2, beta=0., loc=0., scale=1.):
-    print(alpha)
-    r = levy_stable.rvs(alpha=alpha, beta=beta, loc=loc, scale=scale)
+# takes two common parameters and dictionary with 4 parameters needed for levy_stable.rvs function
+def random_force_levy(D, h, records, args):
+    r = levy_stable.rvs(alpha=args['alpha'], beta=args['beta'], loc=args['location'], scale=args['scale'], size=records)
     return (h * D) ** 0.5 * r  # losowa siła
 
 def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0.01, records=50000, model_force=model_force_square, random_force=random_force_gauss, **force_params):
     t = 0
     # losujemy czy kanał początkowo jest otwarty czy zamknięty
     b = choice([closed, opened])
-    x = np.array([model_force(0, a, b[0]) * delta_t + random_force(D, delta_t, force_params)], dtype=np.float32)
+    random_force_values = random_force(D, delta_t, records, force_params)
+    x = np.array([model_force(0, a, b[0]) * delta_t + random_force_values[0]], dtype=np.float32)
     times = np.array([t], dtype=np.float32)
     # losujemy tau, czyli jak długo kanał jest O/Z
     tau = np.random.exponential(b[1])
@@ -28,9 +30,9 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
         x, (
             x[t - 1] +
             model_force(x[t - 1], a, b[0]) * delta_t +
-            random_force(D, delta_t)
+            random_force_values[t - 1]
             )
-    )
+        )
         times = np.append(times, t * delta_t)
         t += 1
         tau -= 1
