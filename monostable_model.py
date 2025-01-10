1d70a724e709d52bb726d68c2935014b904786b3
Piotr Mika <piotrmika97@protonmail.com>
2025-01-10 10:09:02 +0100

    Changed so function saves file with data and returns data

--- monostable_model.py
+++ monostable_model.py
@@ -16,7 +16,7 @@ def random_force_gauss(D, h, records, args):
     Returns:
         np.array: Array with random force values.
     """
-    return (h * D) ** 0.5 * gauss(0, 1, size=records)  # losowa siła
+    return np.array([(h * D) ** 0.5 * gauss(0, 1) for _ in range(records)])  # losowa siła
 
 def model_force_square(x, a, b):
     """Function calculates force value using square function.
@@ -49,8 +49,7 @@ def random_force_levy(D, h, records, args):
     return (h * D) ** 0.5 * r  # losowa siła
 
 def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0.01, records=50000, model_force=model_force_square, random_force=random_force_gauss, **force_params):
-    """Function, that generates a plot of time series of ion channel model.
-
+    """Function, that generates time series of ion channel model and saves it to csv file.
     Args:
         a (int, optional): One of the coefficients of force. Defaults to 1.
         closed (tuple, optional): Tuple where first item is state of closed ion channel and second is value to be passed to np.random.exponential to generate time the channel is closed. Defaults to (-1, 5000).
@@ -61,16 +60,22 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
         model_force (func, optional): Parameter to pass model force. Defaults to model_force_square.
         random_force (func, optional): Parameter to pass random force that generates noise. Defaults to random_force_gauss.
         **force_params: Additional parameters for random force function.
+
+    Returns:
+        np.array: Array with data.
     """
-    
+    data = []
     t = 0
-    # losujemy czy kanał początkowo jest otwarty czy zamknięty
+    # generating first state of ion channel (opened/closed)
     b = choice([closed, opened])
     random_force_values = random_force(D, delta_t, records, force_params)
     x = np.array([model_force(0, a, b[0]) * delta_t + random_force_values[0]], dtype=np.float32)
     times = np.array([t], dtype=np.float32)
-    # losujemy tau, czyli jak długo kanał jest O/Z
+    # generating time of next state change (opened/closed)
     tau = np.random.exponential(b[1])
+    t += 1
+    data.append([t, x[0], b[0]])
+    print(x[0], b[0])
     while t < records:
         x = np.append(
         x, (
@@ -80,6 +85,9 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
             )
         )
         times = np.append(times, t * delta_t)
+
+        data.append([t, x[t], b[0]])
+
         t += 1
         tau -= 1
         if(tau <= 0):
@@ -89,6 +97,7 @@ def ion_channel_model(a=1, closed=(-1, 5000), opened=(1, 2500), D=0.5, delta_t=0
                 b=closed
             tau = np.random.exponential(b[1])
 
-    plt.clf()
-    plt.plot(times, x)
-    plt.show()
+    data = np.array(data)
+    # Temporary naming scheme
+    np.savetxt(f'data_{model_force.__name__}_{random_force.__name__}_{D}_{delta_t}_{list(force_params.values()) if isinstance(force_params, dict) else '_'}.csv', data, delimiter=',')
+    return data
